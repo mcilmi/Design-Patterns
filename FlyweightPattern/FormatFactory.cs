5e6f063b6bb76ab51196c3e1a715041acda1f6ef
Maxamed Cilmi <mohamedshabeel@gmail.com>
2020-04-19 11:25:15 +0200

    hide debug statement

--- FlyweightPattern/FormatFactory.cs
+++ FlyweightPattern/FormatFactory.cs
@@ -12,7 +12,7 @@ namespace FlyweightPattern {
             // Key -> Value (Key: Formatting)
             var key = fontFamily.ToString() + fontSize.ToString() + isBold.ToString();
             var hasKey = this._formatting.TryGetValue(key, out var result);
-            Console.WriteLine("key {0} hasKey: {1}", key, hasKey);
+            // Console.WriteLine("key {0} hasKey: {1}", key, hasKey);
             if (hasKey) return result;
             var newFormatting = new Formatting(fontFamily,fontSize, isBold);
             this._formatting.Add(key, newFormatting);
