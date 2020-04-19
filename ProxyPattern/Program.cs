b09d794e69dfdc8efae43a06308ae2e46621ec03
Maxamed Cilmi <mohamedshabeel@gmail.com>
2020-04-19 16:24:20 +0200

    init. Proxy design pattern

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProxyPattern {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
