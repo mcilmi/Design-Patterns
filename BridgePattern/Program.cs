a3282214eddf7d4f864e366402174617cd6045ad
Maxamed Cilmi <mohamedshabeel@gmail.com>
2020-04-19 13:26:57 +0200

    IRemoteControl usage

--- BridgePattern/Program.cs
+++ BridgePattern/Program.cs
@@ -3,7 +3,7 @@
 namespace BridgePattern {
     public class Program {
         public static void Main(string[] args) {
-            var remoteControl = new RemoteControl(new SamsungTv());
+            IRemoteControl remoteControl = new RemoteControl(new SamsungTv());
             remoteControl.TurnOn();
             remoteControl.SetChannel(34);
             remoteControl.SetChannel(27);
